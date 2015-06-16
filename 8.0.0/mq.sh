78232e88442c3e158dd0a4eacddd4fa878c22e54
Dominic Evans <dominic.evans@uk.ibm.com>
2015-06-16 16:29:30 +0100

    Fix issue with monitor() never exiting
    The monitor() loop never seemed to correctly detect the ENDED status
    using a bash regex test.

--- 8.0.0/mq.sh
+++ 8.0.0/mq.sh
@@ -49,23 +49,29 @@ config()
 
 state()
 {
-   dspmq -n | grep ${MQ_QMGR_NAME} | cut -f3 -d"("
+	dspmq -n -m ${MQ_QMGR_NAME} | awk -F '[()]' '{ print $4 }'
 }
 
 monitor()
 {
 	# Loop until "dspmq" says the queue manager is running
-	until [ "`state`" == "RUNNING)" ]; do
+	until [ "`state`" == "RUNNING" ]; do
 		sleep 1
 	done
 	dspmq
 
 	# Loop until "dspmq" says the queue manager is not running any more
-	until [ "`state`" != "RUNNING)" ]; do
+	until [ "`state`" != "RUNNING" ]; do
 		sleep 5
 	done
 
-	until [[ "`state`" =~ ".*(ENDED.*" ]]; do
+	# Wait until queue manager has ended before exiting
+	while true; do
+		STATE=`state`
+		case "$STATE" in
+			ENDED*) break;;
+			*) ;;
+		esac
 		sleep 1
 	done
 	dspmq
