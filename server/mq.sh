1aa074a9998167f78cef3ee2534ba3f93da3537a
arthurbarr <arthur.barr@uk.ibm.com>
2017-02-01 09:56:19 +0000

    Fix copyright information

--- server/mq.sh
+++ server/mq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # -*- mode: sh -*-
-# © Copyright IBM Corporation 2015, 2016
+# © Copyright IBM Corporation 2015, 2017
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
