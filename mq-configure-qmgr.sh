17eb79e944acefd3189ca665e2f94562eb0981f6
Rob Parker <PARROBE@uk.ibm.com>
2017-06-21 13:34:28 +0100

    Fix issue 82

--- mq-configure-qmgr.sh
+++ mq-configure-qmgr.sh
@@ -15,7 +15,11 @@
 # limitations under the License.
 
 # Turn off script failing here because of listeners failing the script
+set +e
 
 for MQSC_FILE in $(ls -v /etc/mqm/*.mqsc); do
   runmqsc ${MQ_QMGR_NAME} < ${MQSC_FILE}
 done
+
+# Turn back on script failing here
+set -e
